c213813914c3723136121f2fdf3dc88d066a6e42
Hisham Elsheshtawy <hisham.elsheshtawy@gmail.com>
2014-08-18 20:15:33 +0300

    still fixing deadline issue

--- js/views/todos.js
+++ js/views/todos.js
@@ -29,7 +29,7 @@ var app = app || {};
       this.toggleVisible();
       var nw = moment();
       var deb = new moment(this.model.get('dueDate'));
-      var def = moment.duration(Math.abs(nw - deb)).asMinutes();
+      var def = Math.abs(moment.duration((nw - deb).asMinutes())/1000;
       if( def < 10){
          this.$el.addClass('list-group-item-danger');
        }                                       
